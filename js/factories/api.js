461e1a1ae555207db06a841a45761654c1181365
Martha Fairbanks <marthafairbanks@gmail.com>
2016-07-20 03:09:30 -0400

    user end finished

--- js/factories/api.js
+++ js/factories/api.js
@@ -5,13 +5,18 @@
 		 .factory('API', function($http, Backand) {
 
 		 	var postGame = function(data){
+		 		console.log(data);
 		 		return $http ({
 				  method: 'POST',
 				  data:data,
 				  url: Backand.getApiUrl() + '/1/objects/games',
 				});
 		 	};
-
+		 	
+		 	return {
+		 		postGame: postGame,
+                 
+             };
 		 
 		 });
 })();
